a56d55ce7c1a7bac1f1230d3b30f5caba71c7b7b
Mohammed Hrima <mhrima@e1r10p2.1337.ma>
2023-12-29 09:20:32 +0100

    2023-12-29 09-20-32

--- compiler/0.random/operations/div.c
+++ compiler/0.random/operations/div.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
 
-int main()
-{
-    long a = 12;
-    long c = 17 + a;
-    long d = c  / a;
-    return d;
+int remainder, divisor;
+
+int division(int tempdividend, int tempdivisor) {
+    int quotient = 1;
+
+    if (tempdivisor == tempdividend) {
+        remainder = 0;
+        return 1;
+    } else if (tempdividend < tempdivisor) {
+        remainder = tempdividend;
+        return 0;
+    }   
+
+    do{
+
+        tempdivisor = tempdivisor << 1;
+        quotient = quotient << 1;
+
+     } while (tempdivisor <= tempdividend);
+
+
+     /* Call division recursively */
+    quotient = quotient + division(tempdividend - tempdivisor, divisor);
+
+    return quotient;
+} 
+
+
+int main() {
+    int dividend;
+
+    printf ("\nEnter the Dividend: ");
+    scanf("%d", &dividend);
+    printf("\nEnter the Divisor: ");
+    scanf("%d", &divisor);   
+
+    printf("\n%d / %d: quotient = %d", dividend, divisor, division(dividend, divisor));
+    printf("\n%d / %d: remainder = %d", dividend, divisor, remainder);
+    getch();
 }
--- compiler/file.s
+++ compiler/file.s
@@ -1,33 +1,40 @@
 .section	.note.GNU-stack,"",@progbits
 .intel_syntax noprefix
 .text
-	.globl	main
-
-.LC0: .long	  1076953088
-.LC1: .long	  1075576832
-.LC2: .long	  1076101120
-.LC3: .string "%f + %f = %f\n"
+.globl	main
 
 main:
-   push     rbp
-	mov	rbp, rsp
+   push    rbp
+   mov     rbp, rsp
+   sub     rsp, 30
+   /* declare x */
+   mov     QWORD PTR -4[rbp], 0
+   /* assign to x */
+   movss   xmm1, DWORD PTR FLT2[rip]
+   movss   DWORD PTR -4[rbp], xmm1
+   /* declare y */
+   mov     QWORD PTR -8[rbp], 0
+   /* assign to y */
+   movss   xmm1, DWORD PTR FLT3[rip]
+   movss   DWORD PTR -8[rbp], xmm1
+   /* declare z */
+   mov     QWORD PTR -12[rbp], 0
+   movss   xmm1, DWORD PTR -8[rbp]
+   addss   xmm1, DWORD PTR FLT4[rip]
+   movss   DWORD PTR -16[rbp], xmm1
+   movss   xmm1, DWORD PTR -4[rbp]
+   addss   xmm1, DWORD PTR -16[rbp]
+   movss   DWORD PTR -20[rbp], xmm1
+   /* assign to z */
+   movss   xmm1, DWORD PTR -20[rbp]
+   movss   DWORD PTR -12[rbp], xmm1
 
-	/* a */
-	mov		rax, QWORD PTR .LC0[rip]
-	movq     xmm2, rax
-   
-	/* b */
-	mov		rax, QWORD PTR .LC1[rip]
-	movq	   xmm1, rax
+   leave
+   ret
 
-    /* c */
-	mov		rax, QWORD PTR .LC2[rip]
-	movq	   xmm0, rax
-	
-	mov		eax, 1
-	call	   printf@PLT
-	xor		eax, eax
-   
-	leave
-	ret
-
+FLT2:/* 2.000000 */
+   .long  1073741824
+FLT3:/* -1.000000 */
+   .long  3212836864
+FLT4:/* 5.000000 */
+   .long  1084227584
